2c3afd04ca4bcdde8d45ed3af1e0d859c50d0f3d
Aaron Janes <Aaron.Janes@gmail.com>
2021-02-23 13:50:02 -0700

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# google_clone
+# POC GOOGLE CLONE USING REACT
+
+## To build a reusable google clone for future projects
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/search">{/* <SearchPage /> */}</Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+export default App;
