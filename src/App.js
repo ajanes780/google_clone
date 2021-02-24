fd504ef9eee614028b45079891b719322f89107b
Aaron Janes <Aaron.Janes@gmail.com>
2021-02-23 20:13:29 -0700

    add google api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SearchPage from "../src/pages/SearchPage";
+require("dotenv").config();
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Switch>
-          <Route path="/search">{/* <SearchPage /> */}</Route>
+          <Route path="/search">{<SearchPage />}</Route>
           <Route path="/">
             <Home />
           </Route>
---
+++ src/pages/useGoogleSearch.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+const API_KEY = process.env.REACT_APP_GOOGLEAPIKEY;
+const CONTEXT_KEY = process.env.REACT_APP_SEARCHENGINEID;
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
+
+    fetchData();
+  }, [term]);
+
+  return { data };
+};
+export default useGoogleSearch;
